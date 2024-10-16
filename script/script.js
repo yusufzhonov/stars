1cac7c759947b5053bc840a5ff22de32de036cd5
yusufzhonov <yusufzonovmikey@gmail.com>
2024-10-16 23:40:33 +0500

    edited script.js

--- script/script.js
+++ script/script.js
@@ -1,11 +1,11 @@
-let stars = "*";
+let a = '*';
 
 for (let i = 1; i <= 6; i++) {
-  console.log(stars);
-  stars += "*";
+  console.log(a);
+  a += '*';
 };
 
-for (let i = 5; i >= 1; i--) {
-  stars = stars.substring(0, stars.length - 1);
-  console.log(stars);
+for (let i = 5; i >= 0; i--) {
+  a = a.substring(0, a.length - 1);
+  console.log(a);
 };
